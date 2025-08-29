80c0814f31769604b264fb658fc012205044f3ec
Sathish Ravi <ravisathish47@gmail.com>
2025-08-29 22:46:44 +0530

    Profile image added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import myImage from "./assets/MySelf.jpeg";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={myImage} alt="Example" width="100" />
       </div>
-      <h1>My blog is in beta—launching soon!!!</h1>
+      <h1>Just a little longer… my blog is in beta and preparing for launch!</h1>
     </>
   )
 }
